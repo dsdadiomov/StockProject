676e5270f24f0a079001ce9b3c8cad7e8e0b4e74
dsdadiomov <98002949+dsdadiomov@users.noreply.github.com>
2022-04-18 17:46:26 -0700

    Add files via upload

---
+++ Base_Folder/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='stockhelper',
+      version='0.1',
+      description='assists with day trading',
+      author='David Dadiomov',
+      author_email='dadiomov.d@northeastern.edu',
+      license='Artistic 2.0',
+      packages=['stock'],
+      zip_safe=False)
